e079c910b91d3960f83be4201d531b9eab5ce7d6
Antoine Martin <antoine97.martin@gmail.com>
2020-04-11 17:24:30 +0200

    Setup journal

--- init.el
+++ init.el
@@ -137,7 +137,8 @@
        ;;nim               ; python + lisp at the speed of c
        ;;nix               ; I hereby declare "nix geht mehr!"
        ;;ocaml             ; an objective camel
-       org               ; organize your plain life in plain text
+       (org               ; organize your plain life in plain text
+        +journal)
        ;;perl              ; write code no one else can comprehend
        ;;php               ; perl's insecure younger brother
        ;;plantuml          ; diagrams for confusing people more
