a769825f12ce3b15bcd04a18e2f914cebb0f4307
Antoine Martin <antoine97.martin@gmail.com>
2020-07-07 16:18:01 +0200

    Add fish support

--- init.el
+++ init.el
@@ -156,7 +156,8 @@
        (rust +lsp)              ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
        ;;scala             ; java, but good
        ;;scheme            ; a fully conniving family of lisps
-       sh                ; she sells {ba,z,fi}sh shells on the C xor
+       (sh                ; she sells {ba,z,fi}sh shells on the C xor
+        +fish)
        ;;sml
        ;;solidity          ; do you need a blockchain? No.
        ;;swift             ; who asked for emoji variables?
